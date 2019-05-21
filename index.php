a08998c3df90438193e0bdf31412962a8d2ebaed
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:59:20 +0800

    modify

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <html>
  <head>
   <title>qz test</title>
-  <meta charset="UTF-8">
  </head>
 <body>
-<p><a href="/cctv_list.php"><h1>CCTVµÁ ”∆µµ¿</h1></a></p>
+<p><a href="/cctv_list.php"><h1>CCTV list</h1></a></p>
 </body>
 </html>
