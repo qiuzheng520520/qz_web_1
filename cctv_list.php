d4de99f1ec5719a10473235591e8166521e5969d
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:43:01 +0800

    modfiy

--- cctv_list.php
+++ cctv_list.php
@@ -3,6 +3,7 @@
   <title>cctv list</title>
  </head>
 <body>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>
 <p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv2_1/index.m3u8">CCTV2</a></p>
 <?php
 echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>'
