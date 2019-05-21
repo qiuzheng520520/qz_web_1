9ed404b47458ab0b9c44f480d05f4eb46e9eb5fd
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:27:59 +0800

    modify

--- cctv_list.php
+++ cctv_list.php
@@ -4,11 +4,7 @@
  </head>
 <body>
 <?php
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>'
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv2_1/index.m3u8">CCTV2</a></p>'
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv3_1/index.m3u8">CCTV3</a></p>'
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv4_2/index.m3u8">CCTV4</a></p>'
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv5_1/index.m3u8">CCTV5</a></p>'
+echo '<p><a href="/cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>'
 ?>
 </body>
 </html>
