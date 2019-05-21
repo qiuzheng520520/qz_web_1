4393743f0fd1aedde0b8174e614f468e600dd8b4
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:19:49 +0800

    modify

--- cctv_list.php
+++ cctv_list.php
@@ -1,11 +1,14 @@
 <html>
+ <head>
+  <title>cctv list</title>
+ </head>
 <body>
 <?php
-<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>
-<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv2_1/index.m3u8">CCTV2</a></p>
-<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv3_1/index.m3u8">CCTV3</a></p>
-<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv4_2/index.m3u8">CCTV4</a></p>
-<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv5_1/index.m3u8">CCTV5</a></p>
+echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>'
+echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv2_1/index.m3u8">CCTV2</a></p>'
+echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv3_1/index.m3u8">CCTV3</a></p>'
+echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv4_2/index.m3u8">CCTV4</a></p>'
+echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv5_1/index.m3u8">CCTV5</a></p>'
 ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
   <title>qz test</title>
  </head>
 <body>
-<link rel="alternate" type="text/html" lang="zh-cn" hreflang="zh-cn" href="/cctv_list.php" title="CCTV list" />
 <?php
 echo '<p><a href="/cctv_list.php">CCTV list</a></p>'
 ?>
