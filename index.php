6bcd403672b2933d5486c2aafe4fb2cc8ac5b6dd
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:12:13 +0800

    modify

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 <link rel="alternate" type="text/html" lang="zh-cn" hreflang="zh-cn" href="/cctv_list.php" title="CCTV list" />
 <?php
-echo "123456789"
+echo '<link rel="alternate" type="text/html" lang="zh-cn" hreflang="zh-cn" href="/cctv_list.php" title="CCTV list" />'
 //<p><a href="/cctv_list.php">CCTVµÁ ”∆µµ¿</a></p>
 ?>
 </body>
