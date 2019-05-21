9833b1f2a78d892d8678324638d343a26b4b2dc2
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:05:13 +0800

    modify

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 <?php
-<link rel="alternate" type="text/html" lang="zh-cn" hreflang="zh-cn" href="/cctv_list.php" title="CCTV电视频道" />
+<link rel="alternate" type="text/html" lang="zh-cn" hreflang="zh-cn" href="/cctv_list.php" title="CCTV list" />
 //<p><a href="/cctv_list.php">CCTV电视频道</a></p>
 ?>
 </body>
