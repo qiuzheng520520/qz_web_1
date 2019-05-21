223830d22184490da3f1724644998947f6216fa6
qiu zheng <zheng.qiu@foxmail.com>
2019-05-21 23:48:02 +0800

    modify

--- cctv_list.php
+++ cctv_list.php
@@ -5,8 +5,17 @@
 <body>
 <p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>
 <p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv2_1/index.m3u8">CCTV2</a></p>
-<?php
-echo '<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv1_2/index.m3u8">CCTV1</a></p>'
-?>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv3_1/index.m3u8">CCTV3</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv4_1/index.m3u8">CCTV4</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv5_1/index.m3u8">CCTV5</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv6_1/index.m3u8">CCTV6</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv7_1/index.m3u8">CCTV7</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv8_1/index.m3u8">CCTV8</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv9_1/index.m3u8">CCTV9</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv10_1/index.m3u8">CCTV10</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv11_1/index.m3u8">CCTV11</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv12_1/index.m3u8">CCTV12</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv13_1/index.m3u8">CCTV13</a></p>
+<p><a href="http://cctvalih5c.v.myalicdn.com/live/cctv15_1/index.m3u8">CCTV15</a></p>
 </body>
 </html>
