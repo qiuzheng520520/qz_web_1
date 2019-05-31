0baedfb27ce3c05c6fd5c75d547cb078d0921b78
qiu zheng <zheng.qiu@foxmail.com>
2019-05-31 22:40:46 +0800

    modify

--- live_xiaopijiu.php
+++ live_xiaopijiu.php
@@ -39,8 +39,10 @@ $output=str_replace('\\', '', $output);
   if($str=='' or !preg_match('/(http|www\.|@)/i', $str)) { return $str; }
   $lines = explode("play_url", $str); $new_text = '';
   while (list($k,$l) = each($lines)) {
-    $l = preg_replace("/(http:\/\/[^\"]+flv?)/i",
-      "<a href=\"\\1\">\\1</a>", $l);
+    // $l = preg_replace("/(http:\/\/[^\"]+flv?)/i",
+      // "<a href=\"\\1\">\\1</a>", $l);
+	$l = preg_replace("/(http:\/\/[^\"]+flv?)/i",
+    "<EMBED SRC=\"\\1\" autostart=false loop=false width=350 height=250>", $l);
 	$l = preg_replace("/(http:\/\/[^\"]+png?)/i",
 	 "<img src=\"\\1\"  alt=\"aaa\" height=\"200\" width=\"200\" />",$l);
 	$l = preg_replace("/(http:\/\/[^\"]+jpg?)/i",
