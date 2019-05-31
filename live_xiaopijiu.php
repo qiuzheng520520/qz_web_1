112411a0dc1a7f8140938157dbe0aa6875b61b88
qiu zheng <zheng.qiu@foxmail.com>
2019-05-31 19:53:42 +0800

    mdoify

--- live_xiaopijiu.php
+++ live_xiaopijiu.php
@@ -39,18 +39,8 @@ $output=str_replace('\\', '', $output);
   if($str=='' or !preg_match('/(http|www\.|@)/i', $str)) { return $str; }
   $lines = explode("play_url", $str); $new_text = '';
   while (list($k,$l) = each($lines)) {
-    // replace links:
-    // $l = preg_replace("/([ \t]|^)www\./i", "\\1http://www.", $l);
-    // $l = preg_replace("/([ \t]|^)ftp\./i", "\\1ftp://ftp.", $l);
     $l = preg_replace("/(http:\/\/[^ )\r\n!]+)/i",
       "<a href=\"\\1\">\\1</a>", $l);
-    // $l = preg_replace("/(https:\/\/[^ )\r\n!]+)/i",
-      // "<a href=\"\\1\">\\1</a>", $l);
-    // $l = preg_replace("/(ftp:\/\/[^ )\r\n!]+)/i",
-      // "<a href=\"\\1\">\\1</a>", $l);
-    // $l = preg_replace(
-      // "/([-a-z0-9_]+(\.[_a-z0-9-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)+))/i",
-      // "<a href=\"mailto:\\1\">\\1</a>", $l);
     $new_text .= $l."\n";
   }
   return $new_text;
